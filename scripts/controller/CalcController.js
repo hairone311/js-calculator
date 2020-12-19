b441a90e1612d9600141320dabef5298667e8a56
Eliseu Hairone <eliseuarviscoh@gmail.com>
2020-12-19 08:55:48 +0200

    Funcionalidades do Botão Porcento e Igual

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -17,6 +17,8 @@ class CalcController {
         setInterval(() => {
             this.setDisplayDateTime();
         }, 1000);
+
+        this.setLastNumberToDisplay();
     }
 
     addEventListenerAll(element, events, fn) {
@@ -27,10 +29,12 @@ class CalcController {
 
     clearAll() {
         this._operation = [];
+        this.setLastNumberToDisplay();
     }
 
     clearEntry() {
         this._operation.pop();
+        this.setLastNumberToDisplay();
     }
 
     getLastOperation() {
@@ -56,11 +60,25 @@ class CalcController {
     }
 
     calc() {
-        let last = this._operation.pop();
+
+        let last = '';
+
+        if (this._operation.length > 3) {
+            last = this._operation.pop();
+        }
 
         let result = eval(this._operation.join(""));
 
-        this._operation = [result, last];
+        if (last == "%") {
+            result /= 100;
+            this._operation = [result];
+        } else {
+            this._operation = [result];
+
+            if (last) {
+                this._operation.push(last);
+            }
+        }
 
         // Atualizar display
         this.setLastNumberToDisplay();
@@ -74,6 +92,10 @@ class CalcController {
                 lastNumber = this._operation[i];
                 break;
             }
+        }
+
+        if (!lastNumber) {
+            lastNumber = 0;
         }
 
         this.displayCalc = lastNumber;
@@ -135,7 +157,7 @@ class CalcController {
                 this.addOperation('%');
                 break;
             case 'igual':
-
+                this.calc();
                 break;
             case 'ponto':
                 this.addOperation('.');
