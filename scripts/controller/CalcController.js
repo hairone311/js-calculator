60993e96307a38112463010fdb0a50508c340cdf
Eliseu Hairone <eliseuarviscoh@gmail.com>
2020-12-21 19:18:42 +0200

    Audio Click

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,8 @@
 class CalcController {
 
     constructor() {
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -45,10 +47,30 @@ class CalcController {
 
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
+
+        document.querySelectorAll('btn-ac').forEach(btn => {
+            btn.addEventListener('dbclick', e => {
+                this.toggleAudio();
+            });
+        });
+    }
+
+    toggleAudio() {
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio() {
+        if (this._audioOnOff) {
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyboard() {
+
         document.addEventListener('keyup', e => {
+
+            this.playAudio();
 
             switch (e.key) {
                 case 'Escape':
@@ -250,9 +272,13 @@ class CalcController {
     }
 
     execBtn(value) {
+
+        this.playAudio();
+
         switch (value) {
             case 'ac':
                 this.clearAll();
+                this.toggleAudio();
                 break;
             case 'ce':
                 this.clearEntry();
