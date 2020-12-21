0f6cd5ebf029620d4e0c35c2604fc3e0dac2a99a
Eliseu Hairone <eliseuarviscoh@gmail.com>
2020-12-21 18:04:42 +0200

    Eventos de Teclado

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -11,6 +11,7 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvenet();
+        this.initKeyboard();
     }
 
     initialize() {
@@ -21,6 +22,47 @@ class CalcController {
         }, 1000);
 
         this.setLastNumberToDisplay();
+    }
+
+    initKeyboard() {
+        document.addEventListener('keyup', e => {
+
+            switch (e.key) {
+                case 'Escape':
+                    this.clearAll();
+                    break;
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+                case '+':
+                case '-':
+                case '/':
+                case '*':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+            }
+        });
     }
 
     addEventListenerAll(element, events, fn) {
@@ -59,8 +101,6 @@ class CalcController {
 
         if (this._operation.length > 3) {
             this.calc();
-        } else {
-
         }
     }
 
