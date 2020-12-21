733da3846b93002964d6e103a9a7916a2519d389
Eliseu Hairone <eliseuarviscoh@gmail.com>
2020-12-21 17:21:43 +0200

    Correções de Bugs com o Ponto

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -148,7 +148,7 @@ class CalcController {
                 this.pushOperation(value);
             } else {
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 // Atualizar display
                 this.setLastNumberToDisplay();
@@ -162,6 +162,10 @@ class CalcController {
 
     addDot() {
         let lastOperation = this.getLastOperation();
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) {
+            return;
+        }
 
         if (this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.');
