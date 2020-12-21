63b651acd86cc606fa7c908770fb24ebf3703466
Eliseu Hairone <eliseuarviscoh@gmail.com>
2020-12-21 19:38:50 +0200

    Valor maximo do display

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -368,6 +368,12 @@ class CalcController {
     }
 
     set displayCalc(value) {
+
+        if (value.toString().length > 10) {
+            this.setError();
+            return false;
+        }
+
         this._displayCalcEl.innerHTML = value;
     }
 
